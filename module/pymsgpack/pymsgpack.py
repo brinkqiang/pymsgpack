2512261b4fe4050e97e864b017552f18057b97fe
brinkqiang <brinkqiang@gmail.com>
2022-10-24 19:07:23 +0800

    add

--- module/pymsgpack/pymsgpack.py
+++ module/pymsgpack/pymsgpack.py
@@ -1,14 +1,14 @@
 # -*- coding: utf-8 -*-
 from pymsgpack import *
 
-a = CPlayerInfoData()
-a.test()
-b = a.to_msgpack()
-
-c = CPlayerInfoData()
-c.from_msgpack(b)
-d = c.to_msgpack()
-assert(b == d)
+#a = CPlayerInfoData()
+#a.test()
+#b = a.to_msgpack()
+#
+#c = CPlayerInfoData()
+#c.from_msgpack(b)
+#d = c.to_msgpack()
+#assert(b == d)
 
 a = CMsgPackLoader()
 
